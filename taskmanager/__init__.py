974796e2be05f9cda9cde7aa372cf332b08f0a81
struk49 <>
2023-06-20 14:55:24 +0000

    created db intergrated db models and schema

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
 
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLAlCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
