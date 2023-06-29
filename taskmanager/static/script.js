297895e1f994ac6210d2a04f2bc3af18f128f1fa
struk49 <>
2023-06-29 09:48:19 +0000

    added env.py and style.css

--- taskmanager/static/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // sidebar initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
-});
